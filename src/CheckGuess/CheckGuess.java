bf3da9a6032a9f3b4b79815991537125e178e4e1
Bethany Burrows <jedcburrows@gmail.com>
2024-05-16 16:13:30 +0100

    Completed logic to check if user has guessed the word

--- src/CheckGuess/CheckGuess.java
+++ src/CheckGuess/CheckGuess.java
@@ -4,22 +4,14 @@ public class CheckGuess {
 
     public static boolean compareGuess(char guessedLetter, String chosenWord, String[] hiddenWord) {
         if(chosenWord.indexOf(guessedLetter) != -1) {
-            System.out.println("Letter exists in word");
             int index = chosenWord.indexOf(guessedLetter);
             while (index >= 0) {
-                //System.out.println(index);
                 hiddenWord[index] = String.valueOf(guessedLetter);
                 index = chosenWord.indexOf(guessedLetter, index + 1);
             }
             return true;
         } else {
-            System.out.println("This letter does not appear in the word");
             return false;
         }
-        //System.out.println(guessedLetter + " is the guess.  The word to guess is " + chosenWord);
     }
-
-    //Checks if guessed letter exists on chosen word
-    //If yes it should replace all instances of _ in hidden word with the guessed letter and display "Correct"
-    //If not it should deduct one life from the player and display "Incorrect"
 }
--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,27 +1,9 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import static CheckGuess.CheckGuess.compareGuess;
+import static Game.Result.*;
 
 public class Gameplay {
-
-    private String result;
-    private static Boolean isOver;
-
-    public String getResult() {
-        return result;
-    }
-
-    public void setResult(String result) {
-        this.result = result;
-    }
-
-    public static Boolean getOver() {
-        return isOver;
-    }
-
-    public void setOver(Boolean over) {
-        isOver = over;
-    }
 
     public static boolean isGuessNumeric(String guess) {
         try {
@@ -43,9 +25,12 @@ public class Gameplay {
         //Set word to guess
         wordPicker.chooseWord();
 
+        //Store chosen word for easier reference
+        String chosenWord = wordPicker.getChosenWord();
+
         //Use while loop to run game while player has lives left
-        while (playerOne.getLives() > 0) {
-            System.out.println(wordPicker.getChosenWord());
+        while (playerOne.getLives() > 0 && !String.join("", wordPicker.getHiddenWord()).equals(chosenWord)) {
+            //System.out.println(wordPicker.getChosenWord());
             System.out.println(Arrays.toString(wordPicker.getHiddenWord()));
 
             //Initialise scanner to grab user input
@@ -67,13 +52,13 @@ public class Gameplay {
             char guessedLetter = guess.charAt(0);
 
             //Check if guess is valid, and if user has made the same guess before
-            if (isGuessNumeric(guess) || !Character.isLetter(guessedLetter)) {
+            if (isGuessNumeric(guess) || !Character.isLetter(guess.charAt(0))) {
                 System.out.println("Guess must be a letter!");
             } else if (playerOne.getGuessedLetters().contains(guess)) {
                 System.out.println("You have already guessed this letter!");
             } else {
                 //Check if guess is correct
-                if(compareGuess(guessedLetter, wordPicker.getChosenWord(), wordPicker.getHiddenWord())) {
+                if(compareGuess(guessedLetter, chosenWord, wordPicker.getHiddenWord())) {
                     //Inform user of correct guess
                     System.out.println("Correct guess");
                 } else {
@@ -89,7 +74,6 @@ public class Gameplay {
             }
 
         }
-        //End game if lives run out
-        System.out.println("Game Over");
+        result(String.join("", wordPicker.getHiddenWord()).equals(wordPicker.getChosenWord()));
     }
 }
